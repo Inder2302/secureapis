b4108d48f7c160b2f5320c00d51f0d1eeccda4c9
Indrajeet Kalra <Inder@Indrajeets-MacBook-Pro.local>
2020-09-05 19:27:39 +1000

    Initial commit

---
+++ src/main/java/com/ijkalra/secureapis/SecureapisApplication.java
@@ -0,0 +1,13 @@
+package com.ijkalra.secureapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureapisApplication.class, args);
+	}
+
+}
